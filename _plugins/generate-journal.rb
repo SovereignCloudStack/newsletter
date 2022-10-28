654dca7c0f1cbc13a84977a9111cd3b09cad8188
Eduard Itrich <eduard@itrich.net>
2022-10-28 14:54:55 +0200

    Finalize style for contributor's journal
    Signed-off-by: Eduard Itrich <eduard@itrich.net>

--- _plugins/generate-journal.rb
+++ _plugins/generate-journal.rb
@@ -3,10 +3,10 @@ require 'securerandom'
 require 'pathname'
 
 module Jekyll
-  class Mailing < Document
+  class JournalMailing < Document
     def initialize(document, dest)
       @data = document.data
-      self.data['layout'] = "mailing"
+      self.data['layout'] = "journal/mailing"
       self.data['issue'] = document.basename_without_ext
       self.data['multipart_boundary'] = SecureRandom.hex
       self.data['html_content'] = document.output
@@ -17,12 +17,12 @@ module Jekyll
     def destination(dest)
       path = Pathname.new(dest) + "mailing.eml"
     end
-  end  
-  
-  class Raw < Document
+  end
+
+  class JournalRaw < Document
     def initialize(document, dest)
       @data = document.data
-      self.data['layout'] = "raw"
+      self.data['layout'] = "journal/raw"
       self.data['issue'] = document.basename_without_ext
       @renderer = Jekyll::Renderer.new(document.site, document)
       @output = self.renderer.run
@@ -32,10 +32,10 @@ module Jekyll
       path = Pathname.new(dest) + "raw.txt"
     end
   end
-    
-  Jekyll::Hooks.register :digest, :post_write do |document|
+
+  Jekyll::Hooks.register :journal, :post_write do |document|
     dest = Pathname.new(document.site.dest) + document.collection.label + document.basename_without_ext
-    mailing = Mailing.new(document, dest)
-    raw = Raw.new(document, dest)
-  end  
+    mailing = JournalMailing.new(document, dest)
+    raw = JournalRaw.new(document, dest)
+  end
 end
